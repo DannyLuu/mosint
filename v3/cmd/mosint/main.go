a784fc3d1c1e0e654a29810900d82229f457b00b
DL <hisoka365@gmail.com>
2024-09-20 14:07:49 -0400

    Update main.go
    Update url

--- v3/cmd/mosint/main.go
+++ v3/cmd/mosint/main.go
@@ -3,7 +3,7 @@ Copyright © 2023 github.com/alpkeskin
 */
 package main
 
-import "github.com/alpkeskin/mosint/v3/pkg/engine"
+import "github.com/DannyLuu/mosint/v3/pkg/engine"
 
 func main() {
 	engine.Start()
